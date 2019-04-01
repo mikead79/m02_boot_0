52b5ebb4bada544523c5304dc6e842fbd9f047f2
Mihai <mikead79@yahoo.com>
2019-04-01 21:55:17 +0200

    Funciones clasicas

--- p1.py
+++ p1.py
@@ -4,4 +4,11 @@ def sumaTodos(limitTo):
         resultado += i
     return resultado
 
+def sumaTodosLosCuadrados(limitTo):
+    resultado = 0
+    for i in range(limitTo+1):
+        resultado += i * i
+    return resultado
+
 print(sumaTodos(100))
+print(sumaTodosLosCuadrados(3))
