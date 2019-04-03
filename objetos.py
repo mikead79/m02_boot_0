547f2c45bddd6c985608a6817090312b687a0bd4
Mihai <mikead79@yahoo.com>
2019-04-03 22:23:20 +0200

    Turtle objects

---
+++ objetos.py
@@ -0,0 +1,10 @@
+import turtle
+
+tortuga = turtle.Turtle()
+otraTortuga = turtle.Turtle()
+
+tortuga.shape('turtle')
+tortuga.fd(50)
+otraTortuga.color('green')
+otraTortuga.left(90)
+otraTortuga.fd(50)
